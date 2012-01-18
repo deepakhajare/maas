4fb72ea79b22ba090cdc26d77d0c4eacddcc1f46
Gavin Panella <gavin@gromper.net>
2012-01-18 16:04:51 +0000

    Use an OptimisingTestSuite everywhere.

--- src/maas/testing/runner.py
+++ src/maas/testing/runner.py
@@ -1,9 +1,16 @@
 from subprocess import check_call, PIPE
 from django.test.simple import DjangoTestSuiteRunner
 
+from testresources import OptimisingTestSuite
+
 
 class MaaSTestRunner(DjangoTestSuiteRunner):
     """Custom test runner; ensures that the test database cluster is up."""
+
+    def build_suite(self, test_labels, extra_tests=None, **kwargs):
+        suite = super(MaaSTestRunner, self).build_suite(
+            test_labels, extra_tests, **kwargs)
+        return OptimisingTestSuite(suite)
 
     def setup_databases(self, *args, **kwargs):
         """Fire up the db cluster, then punt to original implementation."""
