d49cc7c610677e4924f2cfdacd013b00e9f103d9
Gavin Panella <gavin@gromper.net>
2012-01-18 16:44:12 +0000

    Use templates/test.py in test_models.

--- src/maasserver/tests/test_models.py
+++ src/maasserver/tests/test_models.py
@@ -1,6 +1,12 @@
-"""
-Test maasserver models.
-"""
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Test maasserver models."""
+
+from __future__ import print_function
+
+__metaclass__ = type
+__all__ = []
 
 from maas.testing import TestCase
 from maasserver.models import Node, MACAddress
