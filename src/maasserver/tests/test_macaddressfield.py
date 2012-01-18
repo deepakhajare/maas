c0ede5d976d2cbb8ca7f66da17d556a24613589a
Gavin Panella <gavin@gromper.net>
2012-01-18 17:05:48 +0000

    Use templates/test.py and maas.testing.TestCase in test_macaddressfield.

--- src/maasserver/tests/test_macaddressfield.py
+++ src/maasserver/tests/test_macaddressfield.py
@@ -1,15 +1,22 @@
-"""
-Test MACAddressField.
-"""
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Test MACAddressField."""
+
+from __future__ import print_function
+
+__metaclass__ = type
+__all__ = []
 
 from django.core.exceptions import ValidationError
-from django.test import TestCase
 
 from maasserver.models import MACAddress, Node
 from maasserver.macaddress import validate_mac
+from maas.testing import TestCase
 
 
 class TestMACAddressField(TestCase):
+
     def make_MAC(self, address):
         """Create a MAC address."""
         node = Node()
