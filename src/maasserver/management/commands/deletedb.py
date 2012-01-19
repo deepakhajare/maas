e8d76a331498b3bdde63993ead478b91080e93a2
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-01-19 12:11:24 +0100

    Make maasdb commands accessible from bin/django.

---
+++ src/maasserver/management/commands/deletedb.py
@@ -0,0 +1,16 @@
+from subprocess import check_call
+from sys import stderr
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    """Stop and delete the local development database cluster."""
+
+    help = "Delete the development database cluster."
+
+    def handle(self, *args, **kwargs):
+        if len(args) != 0:
+            raise CommandError("Too many arguments.")
+        check_call(['bin/maasdb', 'delete-cluster', 'db'], stderr=stderr)
+
