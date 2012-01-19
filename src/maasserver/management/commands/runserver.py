df1ecbbd5ebb8c1a3985a138c8f97fd4298b26d4
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-01-19 16:15:58 +0100

    Document.

--- src/maasserver/management/commands/runserver.py
+++ src/maasserver/management/commands/runserver.py
@@ -9,7 +9,15 @@ class Command(BaseRunserverCommand):
     """Customized "runserver" command that wraps the WSGI handler."""
 
     def get_handler(self, *args, **kwargs):
+        """Overridable from `BaseRunserverCommand`: Obtain a WSGI handler."""
         wsgi_handler = BaseRunserverCommand.get_handler(self, *args, **kwargs)
+
+        # Wrap the WSGI handler in an oops handler.  This catches (most)
+        # exceptions bubbling up out of the app, and stores them as
+        # oopses in the directory specified by the OOPS_REPOSITORY
+        # configuration setting.
+        # Django's debug mode causes it to handle exceptions itself, so
+        # don't expect oopses when DEBUG is set to True.
         oops_config = Config()
         oops_repository = DateDirRepo(settings.OOPS_REPOSITORY, 'maas')
         oops_config.publishers.append(oops_repository.publish)
