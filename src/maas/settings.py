dba09ac608f91fd25ed29d107265cb550935807c
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-01-19 17:10:05 +0100

    Review changes.

--- src/maas/settings.py
+++ src/maas/settings.py
@@ -18,7 +18,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 # Location where python-oops should store errors.
-OOPS_REPOSITORY = '/var/log/maas'
+OOPS_REPOSITORY = 'logs'
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
