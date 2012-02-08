99684aef3f870720b84a07c0d190adc2b9da71ac
Gavin Panella <gavin.panella@canonical.com>
2012-02-08 21:12:45 +0000

    Move export_via_xmlrpc() to utils.

--- src/provisioningserver/remote.py
+++ src/provisioningserver/remote.py
@@ -18,34 +18,12 @@ from provisioningserver.interfaces import (
     IProvisioningAPI,
     IProvisioningAPI_XMLRPC,
     )
+from provisioningserver.utils import xmlrpc_export
 from twisted.web.xmlrpc import XMLRPC
 from zope.interface import implements
-from zope.interface.interface import Method
 
 
-def export_via_xmlrpc(iface):
-    """Class decorator to alias methods of a class with an "xmlrpc_" prefix.
-
-    For each method defined in the given interface, the concrete method in the
-    decorated class is copied to a new name of "xmlrpc_%(original_name)s". In
-    combination with :class:`XMLRPC`, and the rest of the Twisted stack, this
-    has the effect of exposing the method via XML-RPC.
-
-    The decorated class must implement `iface`.
-    """
-    def decorate(cls):
-        assert iface.implementedBy(cls), (
-            "%s does not implement %s" % (cls.__name__, iface.__name__))
-        for name in iface:
-            element = iface[name]
-            if isinstance(element, Method):
-                method = getattr(cls, name)
-                setattr(cls, "xmlrpc_%s" % name, method)
-        return cls
-    return decorate
-
-
-@export_via_xmlrpc(IProvisioningAPI)
+@xmlrpc_export(IProvisioningAPI)
 class ProvisioningAPI_XMLRPC(XMLRPC, ProvisioningAPI):
 
     implements(IProvisioningAPI_XMLRPC)
