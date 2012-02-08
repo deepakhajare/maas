e8b89f4dee0b3c0d0560deb3678e7c57b422cb25
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-02-08 22:04:54 +0100

    Make all view tests page tests.

--- src/metadataserver/tests/test_api.py
+++ src/metadataserver/tests/test_api.py
@@ -17,11 +17,7 @@ from metadataserver.api import (
     check_version,
     make_list_response,
     make_text_response,
-    meta_data,
-    metadata_index,
     UnknownMetadataVersion,
-    user_data,
-    version_index,
     )
 
 
@@ -51,68 +47,29 @@ class TestHelpers(TestCase):
 class TestViews(TestCase):
     """Tests for the API views."""
 
-    def fake_request(self):
-        """Fake a request."""
-        # This may have to become more realistic later.  For now, it's
-        # just clearer to say "self.fake_request()" than to say "None."
-        return None
+    def get(self, path):
+        # Root of the metadata API service.
+        metadata_root = "/metadata"
+        return Client().get(metadata_root + path)
 
     def test_metadata_index_shows_latest(self):
-        contents = metadata_index(self.fake_request()).content
-        self.assertIn('latest', contents)
+        self.assertIn('latest', self.get('/').content)
 
     def test_metadata_index_shows_only_known_versions(self):
-        contents = metadata_index(self.fake_request()).content
-        for item in contents.splitlines():
+        for item in self.get('/').content.splitlines():
             check_version(item)
         # The test is that we get here without exception.
         pass
 
     def test_version_index_shows_meta_data_and_user_data(self):
-        contents = version_index(self.fake_request(), 'latest').content
-        items = contents.splitlines()
+        items = self.get('/latest/').content.splitlines()
         self.assertIn('meta-data', items)
         self.assertIn('user-data', items)
 
     def test_meta_data_view_returns_text_response(self):
         self.assertEqual(
-            'text/plain',
-            meta_data(self.fake_request(), 'latest')['Content-Type'])
+            'text/plain', self.get('/latest/meta-data/')['Content-Type'])
 
     def test_user_data_view_returns_text_response(self):
         self.assertEqual(
-            'text/plain',
-            user_data(self.fake_request(), 'latest')['Content-Type'])
-
-
-class TestURLs(TestCase):
-    """Tests to ensure that URLs get directed to the right views.
-
-    This does not test detailed behaviour of the views, only that they get
-    invoked appropriately.
-    """
-
-    def get_page(self, path):
-        """GET a page; return (status code, content)."""
-        response = Client().get(path)
-        return (response.status_code, response.content.strip())
-
-    def test_metadata_index(self):
-        self.assertEqual((200, 'latest'), self.get_page('/metadata/'))
-
-    def test_version_index(self):
-        status_code, content = self.get_page('/metadata/latest/')
-        self.assertEqual(200, status_code)
-        self.assertIn('meta-data', content)
-
-    def test_meta_data(self):
-        status_code, content = self.get_page('/metadata/latest/meta-data/')
-        self.assertEqual(200, status_code)
-        self.assertIn('kernel-id', content)
-
-    def test_user_data(self):
-        # Note that user-data is a file, not a directory, so does not
-        # need a slash at the end.
-        status_code, content = self.get_page('/metadata/latest/user-data')
-        self.assertEqual(200, status_code)
-        self.assertNotIn('user-data', content)
+            'text/plain', self.get('/latest/user-data')['Content-Type'])
