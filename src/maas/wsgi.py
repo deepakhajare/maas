a0915b0a062e9683296cb3105061629a0e676af5
Raphael Badin <raphael.badin@canonical.com>
2012-02-29 18:27:23 +0100

    Add copyright.

--- src/maas/wsgi.py
+++ src/maas/wsgi.py
@@ -1,3 +1,16 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""WSGI Application"""
+
+from __future__ import (
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = []
+
 import os
 
 import django.core.handlers.wsgi
