a9ee5eb61632511cf96ee047471dd7b1bc571fe2
Huw Wilkins <huw.wilkins@canonical.com>
2012-03-06 13:53:42 +1100

    Fixed field_type return and imports

--- src/maasserver/templatetags/field_type.py
+++ src/maasserver/templatetags/field_type.py
@@ -1,11 +1,22 @@
 # Copyright 2012 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+"""Field type template tag."""
+
+from __future__ import (
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = [
+        "field_type"
+    ]
+
 from django import template
 
 register = template.Library()
 
 @register.filter('field_type')
 def field_type(field):
-    type = field.field.widget.__class__.__name__
-    return type
+    return field.field.widget.__class__.__name__
