89349f3f68aa6d647cc8aaaea3e603afa4ce0d9e
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-03-14 09:49:29 +0700

    Review change: easier with urljoin.

--- src/maasserver/provisioning.py
+++ src/maasserver/provisioning.py
@@ -14,6 +14,7 @@ __all__ = [
     ]
 
 from urllib import urlencode
+from urlparse import urljoin
 import warnings
 import xmlrpclib
 
@@ -56,9 +57,7 @@ def get_provisioning_api_proxy():
 
 def get_metadata_server_url():
     """Return the URL where nodes can reach the metadata service."""
-    return (
-        "%s/metadata/"
-        % Config.objects.get_config('maas_url').rstrip('/'))
+    return urljoin(Config.objects.get_config('maas_url'), "metadata/")
 
 
 def compose_metadata(node):
