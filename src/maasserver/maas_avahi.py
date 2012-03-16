1c31c27070a0d17e6cf46e234cd5f9a10e0a0cfc
Dave Walker (Daviey) <DaveWalker@ubuntu.com>
2012-03-16 14:50:54 +0000

    Added nasty transaction code.  Annoyingly this code is executed when doing a syncdb, which is too early in the process.
    I really wish the running signal would land soon. Gah.

--- src/maasserver/maas_avahi.py
+++ src/maasserver/maas_avahi.py
@@ -1,21 +1,31 @@
 # Copyright 2012 Canonical Ltd. This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+from django.db.utils import DatabaseError
+from django.db import transaction
+
 from maasserver.models import Config
 from ZeroconfService import ZeroconfService
 
 class AvahiService(object):
 
+    @transaction.commit_manually
     def get_maas_name(self):
-        site_name = Config.objects.get_config('maas_name')
+        try:
+            site_name = Config.objects.get_config('maas_name')
+            transaction.commit()
+        except DatabaseError, e:
+            site_name = False
+            transaction.rollback()
         return site_name
 
     def __init__(self):
         site_name = "%s MAAS Server" % self.get_maas_name()
-        self.service = ZeroconfService(name=site_name,
-                                       port=80,
-                                       stype="_maas._tcp")
-        self.service.publish()
+        if site_name:
+            self.service = ZeroconfService(name=site_name,
+                                           port=80,
+                                           stype="_maas._tcp")
+            self.service.publish()
 
 
     def maas_title_changed(self, sender, instance, created, **kwargs):
