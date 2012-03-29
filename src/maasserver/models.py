9a4a31dfb7e24db95e729ea5b4aadb14abade83c
Raphael Badin <raphael.badin@canonical.com>
2012-03-29 11:34:07 +0200

    Refactor SSHKey.display_html.

--- src/maasserver/models.py
+++ src/maasserver/models.py
@@ -13,6 +13,7 @@ __all__ = [
     "create_auth_token",
     "generate_node_system_id",
     "get_auth_tokens",
+    "get_html_display_for_key",
     "Config",
     "FileStorage",
     "NODE_STATUS",
@@ -700,7 +701,46 @@ def validate_ssh_public_key(value):
         raise ValidationError("Invalid SSH public key.")
 
 
-MAX_KEY_DISPLAY = 30
+HELLIPSIS = '&hellip;'
+
+
+def get_html_display_for_key(key, size):
+    """Return a compact HTML representation of this key with a boundary on
+    the size of the resulting string.
+
+    A key typically looks like this: 'key_type key_string comment'.
+    What we want here is display the key_type and, if possible (i.e. if it
+    fits in the boundary that `size` gives us), the comment.  If possible we
+    also want to display a truncated key_string.  If the comment is too big
+    to fit in, we simply display a cropped version of the whole string.
+
+    :param key: The key for which we want an HTML representation.
+    :type name: basestring
+    :param size: The maximum size of the representation.
+    :type size: int
+    :return: The HTML representation of this key.
+    :rtype: basestring
+    """
+    key = key.strip()
+    key_parts = key.split(' ', 2)
+
+    if len(key_parts) == 3:
+        key_type = key_parts[0]
+        key_string = key_parts[1]
+        comment = key_parts[2]
+        room_for_key = (
+            size - (len(key_type) + len(comment) + len(HELLIPSIS) + 2))
+        if room_for_key > 0:
+            return '%s %.*s%s %s' % (
+                key_type, room_for_key, key_string, HELLIPSIS, comment)
+
+    if len(key) > size:
+        return '%.*s%s' % (size - len(HELLIPSIS), key, HELLIPSIS)
+    else:
+        return key
+
+
+MAX_KEY_DISPLAY = 50
 
 
 class SSHKey(CommonInfo):
@@ -730,15 +770,7 @@ class SSHKey(CommonInfo):
         :return: The HTML representation of this key.
         :rtype: basestring
         """
-        key = self.key.strip()
-        key_parts = key.split(' ')
-        if len(key_parts) == 3 and len(key_parts[2]) < MAX_KEY_DISPLAY:
-            comment = key_parts[2]
-            return mark_safe(
-                '%s&hellip; %s' % (
-                    key[:MAX_KEY_DISPLAY - len(comment)], comment))
-        else:
-            return mark_safe('%s&hellip;' % key[:MAX_KEY_DISPLAY])
+        return mark_safe(get_html_display_for_key(self.key, MAX_KEY_DISPLAY))
 
 
 class FileStorageManager(models.Manager):
