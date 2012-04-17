2b9868ab4650c1bdf9db21456284a78c99b274cb
Raphael Badin <raphael.badin@canonical.com>
2012-04-17 15:48:09 +0200

    Add absolute_import.

--- src/maasserver/management/commands/clearcache.py
+++ src/maasserver/management/commands/clearcache.py
@@ -4,6 +4,7 @@
 """Django command: clear the cache."""
 
 from __future__ import (
+    absolute_import,
     print_function,
     unicode_literals,
     )
--- src/maasserver/management/commands/generate_api_doc.py
+++ src/maasserver/management/commands/generate_api_doc.py
@@ -4,6 +4,7 @@
 """Django command: generate the API documentation."""
 
 from __future__ import (
+    absolute_import,
     print_function,
     unicode_literals,
     )
