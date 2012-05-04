2dafe90676e0e97d171bb1ca770bd3032cce49ac
Gavin Panella <gavin.panella@canonical.com>
2012-05-04 11:45:33 +0100

    Tests for repr_pid.

--- src/maastesting/services/tests/test_database.py
+++ src/maastesting/services/tests/test_database.py
@@ -12,13 +12,18 @@ from __future__ import (
 __metaclass__ = type
 __all__ = []
 
-from os import path
+from os import (
+    getpid,
+    path,
+    )
 
-from maastesting.testcase import TestCase
 from maastesting.services.database import (
     path_with_pg_bin,
     PG_BIN,
+    repr_pid,
     )
+from maastesting.testcase import TestCase
+from testtools.matchers import StartsWith
 
 
 class TestFunctions(TestCase):
@@ -28,3 +33,14 @@ class TestFunctions(TestCase):
         self.assertEqual(
             PG_BIN + path.pathsep + "/bin:/usr/bin",
             path_with_pg_bin("/bin:/usr/bin"))
+
+    def test_repr_pid_not_a_number(self):
+        self.assertEqual("alice", repr_pid("alice"))
+        self.assertEqual("'alice and bob'", repr_pid("alice and bob"))
+
+    def test_repr_pid_not_a_process(self):
+        self.assertEqual("0 (*unknown*)", repr_pid(0))
+
+    def test_repr_pid_this_process(self):
+        pid = getpid()
+        self.assertThat(repr_pid(pid), StartsWith("%d (" % pid))
