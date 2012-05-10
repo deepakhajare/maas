8ef2e451c4f55643e1d14416f1909628072bc864
Gavin Panella <gavin.panella@canonical.com>
2012-05-10 16:56:57 +0100

    Remove Selenium 1 stuff.

--- src/maasserver/tests/test_sauce.py
+++ src/maasserver/tests/test_sauce.py
@@ -1,7 +1,7 @@
 # Copyright 2012 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""..."""
+"""SauceLabs' Sauce OnDemand fixtures."""
 
 from __future__ import (
     absolute_import,
@@ -14,69 +14,11 @@ __all__ = [
     "TestSauce",
     ]
 
-from getpass import (
-    getpass,
-    getuser,
-    )
-import json
 from os import environ
-from textwrap import dedent
-from unittest import SkipTest
-from urlparse import urlparse
 
 from fixtures import Fixture
 from maastesting.testcase import TestCase
-from selenium import (
-    selenium,
-    webdriver,
-    )
-
-
-class SauceLabsFixture(Fixture):
-
-    env_var = 'UI_TEST_SAUCELABS_URL'
-
-    help_text = dedent("""\
-        Set %s to the URL for SauceLabs or a Sauce Connect instance, including
-        username and access-key, e.g. http://username:access-key@example.com.
-        """ % env_var)
-
-    @classmethod
-    def fromEnvironment(cls, start_url="http://localhost/"):
-        url = environ.get(cls.env_var)
-        if url is None:
-            raise SkipTest(cls.help_text)
-        urlparts = urlparse(url)
-        return cls(
-            start_url=start_url,
-            username=(urlparts.username or getuser()),
-            password=(urlparts.password or getpass()),
-            hostname=(urlparts.hostname or 'localhost'),
-            port=(urlparts.port or 4445))
-
-    def __init__(self, start_url, username, password, hostname, port):
-        super(SauceLabsFixture, self).__init__()
-        self.start_url = start_url
-        self.username = username
-        self.password = password
-        self.hostname = hostname
-        self.port = port
-
-    def setUp(self):
-        super(SauceLabsFixture, self).setUp()
-        settings = {
-            "username": self.username,
-            "access-key": self.password,
-            "os": "Windows 2003",
-            "browser": "firefox",
-            "browser-version": "7",
-            "name": "Testing Selenium 1 in Python at Sauce",
-            }
-        self.browser = selenium(
-            self.hostname, self.port, json.dumps(settings), self.start_url)
-        self.browser.start()
-        self.addCleanup(self.browser.stop)
-        self.browser.set_timeout(90000)
+from selenium import webdriver
 
 
 class SauceOnDemandFixture(Fixture):
@@ -125,16 +67,9 @@ class SauceOnDemandFixture(Fixture):
         self.addCleanup(self.browser.quit)
 
 
-class TestSauce(TestCase):
+class TestSauceOnDemandFixture(TestCase):
 
-    def test_SauceLabsFixture(self):
-        saucelabs = SauceLabsFixture.fromEnvironment()
-        browser = self.useFixture(saucelabs).browser
-        browser.open("http://localhost/")
-        browser.wait_for_page_to_load(10000)
-        self.assertTrue(browser.is_text_present("It works!"))
-
-    def test_SauceOnDemandFixture(self):
+    def test_basic_functionality(self):
         # Browser and platform choices
         # <http://saucelabs.com/docs/ondemand/browsers/env/python/se2/linux>.
         capabilities = webdriver.DesiredCapabilities.FIREFOX.copy()
