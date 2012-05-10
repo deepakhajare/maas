1c74958e4fe4f8c1336f2735c17fcc9c80faa95f
Gavin Panella <gavin.panella@canonical.com>
2012-05-10 20:49:39 +0100

    Fixture for starting Sauce Connect.

--- src/maasserver/tests/test_sauce.py
+++ src/maasserver/tests/test_sauce.py
@@ -14,11 +14,96 @@ __all__ = [
     "SauceOnDemandFixture",
     ]
 
-from os import environ
+from os import (
+    environ,
+    path,
+    )
+import subprocess
+from time import sleep
 
-from fixtures import Fixture
+from fixtures import (
+    Fixture,
+    TempDir,
+    )
 from maastesting.testcase import TestCase
 from selenium import webdriver
+from testtools.content import content_from_file
+
+# TODO: Rename password to api_key, or access_key, or something.
+
+
+class SauceConnectFixture(Fixture):
+    """Start up a Sauce Connect server.
+
+    See `Test Internal Sites`_.
+
+    .. _Test Internal Sites:
+      http://saucelabs.com/docs/ondemand/connect
+
+    """
+
+    def __init__(self, jarfile, username, password):
+        """
+        @param jarfile: The path to the ``Sauce-Connect.jar`` file.
+        @param username: The username to connect to SauceLabs with.
+        @param password: The API key for the SauceLabs service.
+        """
+        super(SauceConnectFixture, self).__init__()
+        self.jarfile = path.abspath(jarfile)
+        self.username = username
+        self.password = password
+
+    def setUp(self):
+        super(SauceConnectFixture, self).setUp()
+        self.workdir = self.useFixture(TempDir()).path
+        self.logfile = path.join(self.workdir, "connect.log")
+        self.readyfile = path.join(self.workdir, "ready")
+        self.command = (
+            "java", "-jar", self.jarfile, self.username,
+            self.password, "--readyfile", self.readyfile)
+        self.start()
+        self.addCleanup(self.stop)
+
+    def start(self):
+        with open(path.devnull, "rb") as devnull:
+            with open(self.logfile, "wb", 1) as log:
+                self.addDetail(
+                    path.basename(self.logfile),
+                    content_from_file(self.logfile))
+                self.process = subprocess.Popen(
+                    self.command, stdin=devnull, stdout=log,
+                    stderr=log, cwd=self.workdir)
+        while self.process.poll() is None:  # TODO: Time-out
+            if path.isfile(self.readyfile):
+                break
+            else:
+                sleep(2.0)
+        else:
+            raise subprocess.CalledProcessError(
+                self.process.returncode, self.command)
+
+    def stop(self):
+        if self.process.poll() is None:
+            self.process.terminate()
+            if self.process.wait() not in (0, 143):  # TODO: Why 143?
+                raise subprocess.CalledProcessError(
+                    self.process.returncode, self.command)
+
+
+if __name__ == "__main__":
+    from sys import argv
+    fixture = SauceConnectFixture(*argv[1:4])
+    try:
+        print("Setting up.")
+        fixture.setUp()
+        print("Ready.")
+    finally:
+        try:
+            print("Shutting down.")
+            details = fixture.getDetails()
+            fixture.cleanUp()
+        finally:
+            print(details)
 
 
 class SauceOnDemandFixture(Fixture):
