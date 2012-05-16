64522b4ea42f3315ef80d5a10a6ce1389e028d3a
Julian Edwards <julian.edwards@canonical.com>
2012-05-16 16:57:23 +1000

    Add first test

---
+++ src/provisioningserver/power/poweraction.py
@@ -0,0 +1,18 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Actions for power-related operations."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = []
+
+
+class PowerAction:
+    """Actions for power-related operations."""
+    pass
