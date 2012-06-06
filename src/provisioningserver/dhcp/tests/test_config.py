bec54b4c06c5aaa60a45aea9a7940c46030f60dc
Julian Edwards <julian.edwards@canonical.com>
2012-06-06 13:45:44 +1000

    Add test for missing parameters

--- src/provisioningserver/dhcp/tests/test_config.py
+++ src/provisioningserver/dhcp/tests/test_config.py
@@ -52,3 +52,25 @@ class TestDHCPConfig(TestCase):
 
         expected = self.template % params
         self.assertEqual(expected, output)
+
+    def test_get_config_with_too_few_parameters(self):
+        self.patch(
+            config, "template", self.template
+            )
+
+        params = dict(
+            # subnet is missing
+            subnet_mask="255.0.0.0",
+            next_server="10.0.0.1",
+            broadcast_address="10.255.255.255",
+            dns_servers="10.1.0.1 10.1.0.2",
+            gateway="10.0.0.2",
+            low_range="10.0.0.3",
+            high_range="10.0.0.254")
+
+        e = self.assertRaises(
+            config.DHCPConfigError, config.get_config, **params)
+
+        self.assertEqual(
+            "Passed parameters are missing at least the value for subnet",
+            e.message)
