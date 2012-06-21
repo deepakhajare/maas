74e589dd92a8c6f5bb1b3662bdb02a68d1f90576
Julian Edwards <julian.edwards@canonical.com>
2012-06-21 16:47:34 +1000

    fix lint

--- src/provisioningserver/tasks.py
+++ src/provisioningserver/tasks.py
@@ -63,7 +63,7 @@ def power_off(power_type, **kwargs):
 @task
 def write_tftp_config_for_node(arch, macs, subarch="generic",
                                pxe_target_dir=None, **kwargs):
-    """Write out the TFTP MAC-based config for `node`.
+    """Write out the TFTP MAC-based config for a node.
 
     A config file is written for each MAC associated with the node.
 
@@ -71,7 +71,8 @@ def write_tftp_config_for_node(arch, macs, subarch="generic",
     :type arch: string
     :param macs: An iterable of mac addresses
     :type macs: Iterable of strings
-    :param subarch: The subarchitecture of the node, 
+    :param subarch: The subarchitecture of the node, defaults to "generic" for
+        architectures without sub-architectures.
     :param **kwargs: Keyword args passed to PXEConfig.write_config()
     """
     # TODO: fix subarch when node.py starts modelling sub-architecture for ARM
