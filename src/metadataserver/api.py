3ad38f514ecffe6d44cb3bbc9d1e84a4b95ef49a
Julian Edwards <julian.edwards@canonical.com>
2012-06-25 17:00:12 +1000

    make metadataserver api for netboot switching use the encapsulated method on Node

--- src/metadataserver/api.py
+++ src/metadataserver/api.py
@@ -237,16 +237,14 @@ class VersionIndexHandler(MetadataViewHandler):
         it finishes installing itself.
         """
         node = get_queried_node(request, for_mac=mac)
-        node.netboot = False
-        node.save()
+        node.set_netboot(False)
         return rc.ALL_OK
 
     @api_exported('POST')
     def netboot_on(self, request, version=None, mac=None):
         """Turn on netboot on the node."""
         node = get_queried_node(request, for_mac=mac)
-        node.netboot = True
-        node.save()
+        node.set_netboot(True)
         return rc.ALL_OK
 
 
