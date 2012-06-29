898d4e2ccc4bb62401bebe72e6c0e034e1cc2f8c
Raphael Badin <raphael.badin@canonical.com>
2012-06-29 17:06:08 +0200

    Add doc.

--- src/maasserver/sequence.py
+++ src/maasserver/sequence.py
@@ -22,7 +22,7 @@ from django.db import (
     )
 
 
-BIBINT_MAX = 2 ** 63 - 1
+BIGINT_MAX = 2 ** 63 - 1
 
 INT_MAX = 2 ** 32 - 1
 
@@ -30,13 +30,14 @@ INT_MAX = 2 ** 32 - 1
 class Sequence:
     """SQL sequence."""
 
-    def __init__(self, name, incr=1, minvalue=1, maxvalue=BIBINT_MAX):
+    def __init__(self, name, incr=1, minvalue=1, maxvalue=BIGINT_MAX):
         self.name = name
         self.incr = incr
         self.minvalue = minvalue
         self.maxvalue = maxvalue
 
     def create(self):
+        """Create this sequence in the database."""
         cursor = connection.cursor()
         query = "CREATE SEQUENCE %s" % self.sequence_name
         cursor.execute(
@@ -45,12 +46,18 @@ class Sequence:
         transaction.commit_unless_managed()
 
     def nextval(self):
+        """Return the next value of this sequence.
+
+        :return: The sequence value.
+        :rtype: int
+        """
         cursor = connection.cursor()
         cursor.execute(
             "SELECT nextval(%s)", [self.sequence_name])
         return cursor.fetchone()[0]
 
     def delete(self):
+        """Drop this sequence from the database."""
         cursor = connection.cursor()
         cursor.execute(
             "DROP SEQUENCE %s" % self.sequence_name)
