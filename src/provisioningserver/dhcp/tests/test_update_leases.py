f6dc345a1d2da8b0d68007a282c240b59243018e
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-07-05 16:16:03 +0700

    Skeleton, and test updates to fit it.  No code yet.

--- src/provisioningserver/dhcp/tests/test_update_leases.py
+++ src/provisioningserver/dhcp/tests/test_update_leases.py
@@ -24,7 +24,7 @@ from maastesting.utils import (
     age_file,
     get_write_time,
     )
-from provisioningserver import update_leases
+from provisioningserver.dhcp import update_leases
 
 
 class TestUpdateLeases(TestCase):
@@ -41,14 +41,14 @@ class TestUpdateLeases(TestCase):
         lease_file = self.make_file()
         if age is not None:
             age_file(lease_file, age)
-        self.patch(update_leases, 'LEASES_FILE', lease_file)
+        self.patch(update_leases, 'DHCP_LEASES_FILE', lease_file)
         # TODO: We don't have a lease-file parser yet.  For now, just
         # fake up a "parser" that returns the given data.
         self.patch(update_leases, 'parse_leases', lambda: leases)
         return lease_file
 
     def test_check_lease_changes_returns_True_if_no_state_cached(self):
-        update_leases.record_lease_state()
+        update_leases.record_lease_state(None, None)
         self.fake_leases_file()
         self.assertTrue(update_leases.check_lease_changes())
 
@@ -102,12 +102,13 @@ class TestUpdateLeases(TestCase):
         self.assertFalse(update_leases.check_lease_changes())
 
     def test_update_leases_sends_leases_if_changed(self):
+        update_leases.record_lease_state(None, None)
         send_leases = FakeMethod()
         self.patch(update_leases, 'send_leases', send_leases)
         leases = {
             factory.getRandomIPAddress(): factory.getRandomMACAddress(),
         }
-        update_leases.record_lease_state(leases)
+        self.fake_leases_file(leases)
         update_leases.update_leases()
         self.assertSequenceEqual([leases], send_leases.extract_args())
 
---
+++ src/provisioningserver/dhcp/update_leases.py
@@ -0,0 +1,60 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Send lease updates to the server.
+
+This code runs inside node-group workers.  It watches for changes to DHCP
+leases, and notifies the MAAS server so that it can rewrite DNS zone files
+as appropriate.
+"""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = [
+    'send_leases',
+    'update_leases',
+    ]
+
+
+from celeryconfig import DHCP_LEASES_FILE
+
+
+def check_lease_changes():
+    """Has the DHCP leases file changed in any significant way?"""
+    pass
+
+
+def parse_leases():
+    """Parse the DHCP leases file.
+
+    :return: A dict mapping each leased IP address to the MAC address that
+        it has been assigned to.
+    """
+    pass
+
+
+def record_lease_state(last_change, leases):
+    """Record a snapshot of the state of DHCP leases.
+
+    :param last_change: Modification date on the leases file with the given
+        leases.
+    :param leases: A dict mapping each leased IP address to the MAC address
+        that it has been assigned to.
+    """
+    pass
+
+
+def send_leases(leases):
+    """Send snapshot of current leases to the MAAS server."""
+    pass
+
+
+def update_leases():
+    """Check for DHCP lease updates, and send them to the server if needed.
+    """
+    pass
