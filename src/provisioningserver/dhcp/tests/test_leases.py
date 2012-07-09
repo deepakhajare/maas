9b644b50c609e64a1c2d2f00fdece2f6bfad2dbf
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-07-09 17:06:08 +0700

    Tests for leases parser.  No code yet.

--- src/provisioningserver/dhcp/tests/test_leases.py
+++ src/provisioningserver/dhcp/tests/test_leases.py
@@ -16,6 +16,7 @@ from datetime import (
     datetime,
     timedelta,
     )
+from textwrap import dedent
 
 from maastesting.factory import factory
 from maastesting.fakemethod import FakeMethod
@@ -27,6 +28,7 @@ from maastesting.utils import (
 from provisioningserver.dhcp import leases as leases_module
 from provisioningserver.dhcp.leases import (
     check_lease_changes,
+    parse_leases,
     record_lease_state,
     update_leases,
     upload_leases,
@@ -48,8 +50,16 @@ class TestUpdateLeases(TestCase):
         """Create a leases dict with one, arbitrary lease in it."""
         return {factory.getRandomIPAddress(): factory.getRandomMACAddress()}
 
+    def redirect_parser(self, path):
+        """Make the leases parser read from a file at `path`."""
+        self.patch(leases_module, 'DHCP_LEASES_FILE', path)
+
     def fake_leases_file(self, leases=None, age=None):
-        """Create a fake leases file.
+        """Fake the presence of a leases file.
+
+        This does not go through the leases parser.  It patches out the
+        leases parser with a fake that returns the lease data you pass in
+        here.
 
         :param leases: Dict of leases (mapping IP addresses to MACs).
         :param age: Number of seconds since last modification to leases file.
@@ -62,11 +72,20 @@ class TestUpdateLeases(TestCase):
         if age is not None:
             age_file(leases_file, age)
         timestamp = get_write_time(leases_file)
-        self.patch(leases_module, 'DHCP_LEASES_FILE', leases_file)
-        # TODO: We don't have a lease-file parser yet.  For now, just
-        # fake up a "parser" that returns the given data.
+        self.redirect_parser(leases_file)
         self.patch(leases_module, 'parse_leases', lambda: (timestamp, leases))
         return leases_file
+
+    def write_leases_file(self, contents):
+        """Create a leases file, and cause the parser to read from it.
+
+        This patches out the leases parser to read from the new file.
+
+        :param contents: Text contents for the leases file.
+        """
+        leases_file = self.make_file(
+            contents=dedent(contents).encode('utf-8'))
+        self.redirect_parser(leases_file)
 
     def test_check_lease_changes_returns_tuple_if_no_state_cached(self):
         record_lease_state(None, None)
@@ -186,3 +205,114 @@ class TestUpdateLeases(TestCase):
         except StopExecuting:
             pass
         self.assertIsNone(check_lease_changes())
+
+    def test_parse_leases_parses_lease(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        self.write_leases_file("""\
+            lease %(ip)s {
+                starts 5 2010/01/01 00:00:01;
+                ends never;
+                tstp 6 2010/01/02 05:00:00;
+                tsfp 6 2010/01/02 05:00:00;
+                binding state free;
+                hardware ethernet %(mac)s;
+            }
+            """ % params)
+        leases = parse_leases()
+        self.assertEqual(1, len(leases))
+        lease = leases[0]
+        self.assertEqual(params['ip'], lease.ip)
+        self.assertEqual(params['mac'], lease.mac)
+
+    def test_parse_leases_ignores_incomplete_lease_at_end(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+            'incomplete_ip': factory.getRandomIPAddress(),
+        }
+        self.write_leases_file("""\
+            lease %(ip)s {
+                hardware ethernet %(mac)s;
+            }
+            lease %(incomplete_ip)s {
+                starts 5 2010/01/01 00:00:05;
+            """ % params)
+        leases = parse_leases()
+        self.assertEqual(1, len(leases))
+        self.assertEqual(params['ip'], leases[0].ip)
+
+    def test_parse_leases_ignores_comments(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        self.write_leases_file("""\
+            # Top comment (ignored).
+            lease %(ip)s { # End-of-line comment (ignored).
+                # Comment in lease block (ignored).
+                hardware ethernet %(mac)s;  # EOL comment in lease (ignored).
+            } # Comment right after closing brace (ignored).
+            # End comment (ignored).
+            """ % params)
+        leases = parse_leases()
+        self.assertEqual(1, len(leases))
+        self.assertEqual(params['ip'], leases[0].ip)
+
+    def test_parse_leases_treats_never_as_eternity(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        self.write_leases_file("""\
+            lease %(ip)s {
+                hardware ethernet %(mac)s;
+                ends never;
+            }
+            """ % params)
+        self.assertIsNone(parse_leases()[0].end)
+
+    def test_parse_leases_treats_missing_end_date_as_eternity(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        self.write_leases_file("""\
+            lease %(ip)s {
+                hardware ethernet %(mac)s;
+            }
+            """ % params)
+        self.assertIsNone(parse_leases()[0].end)
+
+    def test_parse_leases_ignores_expired_leases(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        self.write_leases_file("""\
+            lease %(ip)s {
+                hardware ethernet %(mac)s;
+                ends 1 2001/01/01 00:00:00;
+            }
+            """ % params)
+        self.assertSequenceEqual([], parse_leases())
+
+    def test_parse_leases_takes_latest_lease_for_address(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'old_owner': factory.getRandomMACAddress(),
+            'new_owner': factory.getRandomMACAddress(),
+        }
+        self.write_leases_file("""\
+            lease %(ip)s {
+                hardware ethernet %(old_owner)s;
+            }
+            lease %(ip)s {
+                hardware ehternet %(new_owner)s;
+            }
+            """ % params)
+        leases = parse_leases()
+        self.assertEqual(1, len(leases))
+        self.assertEqual(params['new_owner'], leases[0].mac)
