2aa2e7f177e9f2e8345965476f0370092bbcf93e
Raphael Badin <raphael.badin@canonical.com>
2012-07-11 14:08:07 +0200

    Rename command.

--- src/maasserver/tests/test_commands_get_named_conf.py
+++ src/maasserver/tests/test_commands_get_named_conf.py
@@ -1,7 +1,7 @@
 # Copyright 2012 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""Tests for the set_up_dns command."""
+"""Tests for the get_named_conf command."""
 
 from __future__ import (
     absolute_import,
@@ -25,18 +25,18 @@ from testtools.matchers import (
 
 class TestGenerateEnlistmentPXE(TestCase):
 
-    def test_set_up_dns_returns_snippet(self):
+    def test_get_named_conf_returns_snippet(self):
         out = BytesIO()
         stdout = getwriter("UTF-8")(out)
-        call_command('set_up_dns', stdout=stdout)
+        call_command('get_named_conf', stdout=stdout)
         result = stdout.getvalue()
         # Just check that the returned snippet looks all right.
         self.assertIn('include "', result)
 
-    def test_set_up_dns_appends_to_config_file(self):
+    def test_get_named_conf_appends_to_config_file(self):
         file_path = self.make_file()
         call_command(
-            'set_up_dns', edit=True, config_path=file_path)
+            'get_named_conf', edit=True, config_path=file_path)
         self.assertThat(
             file_path,
             FileContains(
