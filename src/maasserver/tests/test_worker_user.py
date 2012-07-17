599f2f79049b865d185d5e5a9640383ce64dbe9a
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-07-17 15:19:33 +0700

    Create node-group workers (with their own system user).  Tests and code, but no data yet.

---
+++ src/maasserver/tests/test_worker_user.py
@@ -0,0 +1,33 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Test for the system user that represents node-group workers."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = []
+
+from django.contrib.auth.models import User
+from maasserver.models import UserProfile
+from maasserver.testing.testcase import TestCase
+from maasserver.worker_user import get_worker_user, user_name
+
+
+class TestNodeGroupUser(TestCase):
+    """Test the special "user" that celery workers use to access the API."""
+
+    def test_get_worker_user_always_returns_same_user(self):
+        self.assertEqual(get_worker_user().id, get_worker_user().id)
+
+    def test_get_worker_user_holds_the_worker_user(self):
+        self.assertIsInstance(get_worker_user(), User)
+        self.assertEqual(user_name, get_worker_user().username)
+
+    def test_worker_user_has_no_profile(self):
+        worker_user = get_worker_user()
+        self.assertRaises(UserProfile.DoesNotExist, worker_user.get_profile)
