1aa62953fa4692f97eda160139244235bf4a32ae
Gavin Panella <gavin.panella@canonical.com>
2012-07-20 18:38:45 +0100

    Set the provisioning configuration file at script start-up.

--- src/provisioningserver/__main__.py
+++ src/provisioningserver/__main__.py
@@ -12,13 +12,30 @@ from __future__ import (
 
 __metaclass__ = type
 
+import argparse
+
+from provisioningserver.config import (
+    get_config_filename,
+    set_config_filename,
+    )
 import provisioningserver.dhcp.writer
 import provisioningserver.pxe.install_bootloader
 import provisioningserver.pxe.install_image
 from provisioningserver.utils import ActionScript
 
 
+class SetConfigFileAction(argparse.Action):
+    """Set the configuration file for use in this process."""
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        set_config_filename(values)
+
+
 main = ActionScript(__doc__)
+main.parser.add_argument(
+    "-c", "--config-file", default=get_config_filename(),
+    metavar="FILENAME", action=SetConfigFileAction,
+    help="Configuration file to load [%(default)s].")
 main.register(
     "generate-dhcp-config",
     provisioningserver.dhcp.writer)
