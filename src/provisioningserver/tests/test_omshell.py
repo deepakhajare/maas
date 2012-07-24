b2547398d9907e0f0896490ee9f4377d6b51b710
Julian Edwards <julian.edwards@canonical.com>
2012-07-24 17:34:26 +1000

    test to check a failing remove()

--- src/provisioningserver/tests/test_omshell.py
+++ src/provisioningserver/tests/test_omshell.py
@@ -118,3 +118,21 @@ class TestOmshell(TestCase):
         self.assertEqual(
             [1, expected_args],
             [recorder.call_count, recorder.extract_args()[0]])
+
+    def test_remove_raises_when_omshell_fails(self):
+        # If the call to omshell doesn't result in output ending in the
+        # text 'obj: <null>' we can be fairly sure this operation
+        # failed.
+        server_address = factory.getRandomString()
+        shared_key = factory.getRandomString()
+        ip_address = factory.getRandomIPAddress()
+        shell = Omshell(server_address, shared_key)
+
+        # Fake a call that results in a failure with random output.
+        random_output = factory.getRandomString()
+        recorder = FakeMethod(result=(random_output, None))
+        self.patch(shell.proc, 'communicate', recorder)
+
+        exc = self.assertRaises(
+            CalledProcessError, shell.remove, ip_address)
+        self.assertEqual(random_output, exc.output)
