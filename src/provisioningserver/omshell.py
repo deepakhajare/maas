650146c4feeb3d53f3381baaf633713d0eafb545
Julian Edwards <julian.edwards@canonical.com>
2012-07-24 16:50:25 +1000

    add test_create_calls_omshell_correctly

--- src/provisioningserver/omshell.py
+++ src/provisioningserver/omshell.py
@@ -16,16 +16,45 @@ __all__ = [
     "Omshell",
     ]
 
+from StringIO import StringIO
+from subprocess import (
+    CalledProcessError,
+    PIPE,
+    Popen,
+    )
+
 
 class Omshell:
     """Wrap up the omshell utility in Python."""
 
     def __init__(self, server_address, shared_key):
-        self.server_address=server_address
-        self.shared_key=shared_key
+        self.server_address = server_address
+        self.shared_key = shared_key
+        self.proc = Popen("omshell", stdin=PIPE, stdout=PIPE)
+
+    def _run(self, stdin):
+        stdout, stderr = self.proc.communicate(stdin)
+        return stdout
 
     def create(self, ip_address, mac_address):
-        pass
+        stdin = (
+            "server %(server)s\n"
+            "key omapi_key %(key)s\n"
+            "connect\n"
+            "new host\n"
+            "set ip-address = %(ip)s\n"
+            "set hardware-address = %(mac)s\n"
+            "set name = %(ip)s\n"
+            "create\n")
+        stdin = stdin % dict(
+            server=self.server_address,
+            key=self.shared_key,
+            ip=ip_address,
+            mac=mac_address)
+
+        output = self._run(stdin)
+        if "hardware-type" not in output:
+            raise CalledProcessError(output)
 
     def remove(self, ip_address):
         pass
