240fd30a7eeaad752af943fec1c767f783a5a1d7
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-07-26 16:11:26 +0700

    Initialize Node.nodegroup when creating master nodegroup.  Cache master.

--- src/maasserver/tests/test_nodegroup.py
+++ src/maasserver/tests/test_nodegroup.py
@@ -76,6 +76,7 @@ class TestNodeGroupManager(TestCase):
         self.assertEqual(nodegroup.api_key, nodegroup.api_token.key)
 
     def test_ensure_master_creates_minimal_master_nodegroup(self):
+        NodeGroup.objects._delete_master()
         self.assertThat(
             NodeGroup.objects.ensure_master(),
             MatchesStructure.fromExample({
@@ -89,16 +90,19 @@ class TestNodeGroupManager(TestCase):
             }))
 
     def test_ensure_master_writes_master_nodegroup_to_database(self):
+        NodeGroup.objects._delete_master()
         master = NodeGroup.objects.ensure_master()
         self.assertEqual(
             master.id, NodeGroup.objects.get(name=master.name).id)
 
     def test_ensure_master_returns_same_nodegroup_every_time(self):
+        NodeGroup.objects._delete_master()
         self.assertEqual(
             NodeGroup.objects.ensure_master().id,
             NodeGroup.objects.ensure_master().id)
 
     def test_ensure_master_does_not_return_other_nodegroup(self):
+        NodeGroup.objects._delete_master()
         self.assertNotEqual(
             NodeGroup.objects.new(
                 factory.make_name('nodegroup'), factory.getRandomIPAddress()),
@@ -112,6 +116,7 @@ class TestNodeGroupManager(TestCase):
         self.assertEqual(ip, NodeGroup.objects.ensure_master().worker_ip)
 
     def test_ensure_master_updates_groupless_nodes(self):
+        NodeGroup.objects._delete_master()
         # This test becomes obsolete (and the failure impossible to test
         # for) once Node.nodegroup is NOT NULL.
         groupless_node = factory.make_node()
