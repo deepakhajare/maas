ad285347fbff0d0b9bb0011ff9bfab92b6130542
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-08-10 22:30:51 +0700

    Module and tests: refresh_worker.

---
+++ src/maasserver/refresh_worker.py
@@ -0,0 +1,14 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+# TODO: Description here.
+"""..."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = []
