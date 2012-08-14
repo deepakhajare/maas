8d333b861d9ff0876eb0cfa99654ec61ad21badf
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-08-14 12:13:16 +0700

    Satisfy end-to-end test.

--- src/maasserver/api.py
+++ src/maasserver/api.py
@@ -124,6 +124,7 @@ from maasserver.models import (
     Node,
     NodeGroup,
     )
+from maasserver.refresh_worker import refresh_worker
 from piston.doc import generate_doc
 from piston.handler import (
     AnonymousBaseHandler,
@@ -880,6 +881,8 @@ class NodeGroupsHandler(BaseHandler):
         does not know its node-group name or API credentials yet) but the
         information will be sent only to the known workers.
         """
+        for nodegroup in NodeGroup.objects.all():
+            refresh_worker(nodegroup)
 
 
 @api_operations
