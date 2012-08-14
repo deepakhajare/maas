d253a04dc776ce08d19c1602890c3e365cfae659
Raphael Badin <raphael.badin@canonical.com>
2012-08-14 15:50:56 +0200

    Use file-based lock in start_up method.

--- src/maasserver/start_up.py
+++ src/maasserver/start_up.py
@@ -15,9 +15,21 @@ __all__ = [
     ]
 
 
+from lockfile import (
+    FileLock,
+    LockFailed,
+    LockTimeout,
+    NotMyLock,
+    )
 from maasserver.dns import write_full_dns_config
 from maasserver.maasavahi import setup_maas_avahi_service
 from maasserver.models import NodeGroup
+
+
+LOCK_FILE_NAME = '/var/lock/maas.start-up'
+
+
+LOCK_TIMEOUT = 60
 
 
 def start_up():
@@ -30,8 +42,24 @@ def start_up():
     This method is called when the MAAS application starts up.
     In production, it's called from the WSGI script so this shouldn't block
     at any costs.  It should simply call very simple methods or Celery tasks.
-    """
 
+    The method will be executed multiple times if multiple processes are used
+    but this method uses file-based locking to ensure that the methods it calls
+    internally are not ran concurrently.
+    """
+    lock = FileLock(LOCK_FILE_NAME)
+    try:
+        lock.acquire(timeout=LOCK_TIMEOUT)
+    except (LockTimeout, NotMyLock, LockFailed):
+        raise
+    else:
+        try:
+            inner_start_up()
+        finally:
+            lock.release()
+
+
+def inner_start_up():
     # Publish the MAAS server existence over Avahi.
     setup_maas_avahi_service()
 
