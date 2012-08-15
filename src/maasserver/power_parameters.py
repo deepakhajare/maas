8f1ed0170d6570c5b103e2e7042b37adc9f03c39
Raphael Badin <raphael.badin@canonical.com>
2012-08-15 15:53:43 +0200

    Make the mac_address a MAC Address field.

--- src/maasserver/power_parameters.py
+++ src/maasserver/power_parameters.py
@@ -32,6 +32,7 @@ __all__ = [
 
 from django import forms
 from maasserver.config_forms import DictCharField
+from maasserver.fields import MACAddressFormField
 from provisioningserver.enum import POWER_TYPE
 
 
@@ -43,7 +44,7 @@ POWER_TYPE_PARAMETERS = {
             [
                 (
                     'mac_address',
-                    forms.CharField(label="MAC Address", required=False)),
+                    MACAddressFormField(label="MAC Address", required=False)),
             ],
             required=False,
             skip_check=True),
