efd918216f021e51aa162798ed8b309f28f74280
Julian Edwards <julian.edwards@canonical.com>
2012-09-03 22:59:27 +1000

    Use atomic-write script instead of the function, so that it can be called with sudo

--- src/provisioningserver/tasks.py
+++ src/provisioningserver/tasks.py
@@ -49,7 +49,6 @@ from provisioningserver.power.poweraction import (
     PowerAction,
     PowerActionFail,
     )
-from provisioningserver.utils import atomic_write
 
 # For each item passed to refresh_secrets, a refresh function to give it to.
 refresh_functions = {
@@ -311,8 +310,9 @@ def write_dhcp_config(**kwargs):
     """
     output = config.get_config(**kwargs).encode("ascii")
     proc = Popen(
-        ["sudo", "maas-provsion", "atomic-write", "--filename", DHCP_CONFIG_FILE])
-    #atomic_write(output, DHCP_CONFIG_FILE, mode=0744)
+        ["sudo", "maas-provsion", "atomic-write", "--filename", DHCP_CONFIG_FILE,
+        "--mode", "744"], stdin=PIPE)
+    proc.communicate(output)
     restart_dhcp_server()
 
 
