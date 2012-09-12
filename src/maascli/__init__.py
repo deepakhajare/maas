20493891bfe7b23b88e5740d9e0eaec44c41faae
Gavin Panella <gavin.panella@canonical.com>
2012-09-12 16:05:08 +0100

    Get the commandant CommandController going.

--- src/maascli/__init__.py
+++ src/maascli/__init__.py
@@ -13,13 +13,27 @@ __metaclass__ = type
 __all__ = []
 
 from glob import iglob
-from os.path import join, dirname
+from os.path import (
+    dirname,
+    join,
+    )
 import sys
-
 
 # Insert all eggs in the current directory into sys.path.
 sys.path[:0] = iglob(join(dirname(__file__), "*.egg"))
 
+from commandant import builtins
+from commandant.controller import CommandController
 
-def main():
-    print("Hello, World!")
+
+def main(argv=sys.argv):
+    controller = CommandController(
+        program_name=__name__, program_version="2.0",
+        program_summary="Control MAAS using its API from the command-line.",
+        program_url="http://maas.ubuntu.com/")
+    # At this point controller.load_path(...) can be used to load commands
+    # from a pre-agreed location on the filesystem, so that the command set
+    # will grow and shrink with the installed packages.
+    controller.load_module(builtins)
+    controller.install_bzrlib_hooks()
+    controller.run(argv[1:])
