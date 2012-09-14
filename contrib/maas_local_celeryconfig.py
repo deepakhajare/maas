54b613f6b0c54ba439d596264874dcde989dec10
Raphael Badin <raphael.badin@canonical.com>
2012-09-14 16:48:17 +0200

    Revert adding the file in contrib/

--- contrib/maas_local_celeryconfig.py
+++
@@ -1,5 +0,0 @@
-"""Celery settings for the MAAS project."""
-
-# Broken connection information.
-# Format: transport://userid:password@hostname:port/virtual_host
-BROKER_URL = 'amqp://guest:guest@localhost:5672//'
