272511563454853c3f5933f561165fb1abf94921
Gavin Panella <gavin.panella@canonical.com>
2012-09-27 00:34:22 +0100

    Turn off all CRUD method for BootImagesHandler.

--- src/maasserver/api.py
+++ src/maasserver/api.py
@@ -1495,6 +1495,8 @@ def pxeconfig(request):
 
 class BootImagesHandler(OperationsHandler):
 
+    create = replace = update = delete = None
+
     @classmethod
     def resource_uri(cls):
         return ('boot_images_handler', [])
