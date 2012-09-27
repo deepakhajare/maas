756d055beb60c3debe4a6888e68876041a0a4c86
Gavin Panella <gavin.panella@canonical.com>
2012-09-27 03:37:18 +0100

    Add bug number to skip.

--- src/maasserver/tests/test_views_nodes.py
+++ src/maasserver/tests/test_views_nodes.py
@@ -506,7 +506,9 @@ class TestGetLongpollContext(TestCase):
         self.patch(nodes_views, 'messaging', messages.get_messaging())
         self.assertEqual({}, get_longpoll_context())
 
-    @skip("Causes test failures in unrelated parts of the test suite.")
+    @skip(
+        "XXX: GavinPanella 2012-09-27 bug=1057250: Causes test "
+        "failures in unrelated parts of the test suite.")
     @uses_rabbit_fixture
     def test_get_longpoll_context(self):
         longpoll = factory.getRandomString()
