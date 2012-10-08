90926627de5381ec46404de85ebd05f927ac2224
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-10-08 21:45:02 +0700

    Docstrings.

--- src/maascli/profile.py
+++ src/maascli/profile.py
@@ -23,14 +23,25 @@ class InvalidProfile(Exception):
 
 
 def get_profile(profiles, profile_name):
-    """Look up the named profile in `profiles`."""
+    """Look up the named profile in `profiles`.
+
+    :param profiles: The result of `ProfileConfig.open()`.
+    :param profile_name: The profile requested by the user.
+    :return: The `ProfileConfig` option for the requested profile.
+    :raise InvalidProfile: Requested profile was not found.
+    """
     if profile_name not in profiles:
         raise InvalidProfile("'%s' is not an active profile." % profile_name)
     return profiles[profile_name]
 
 
 def name_default_profile(profiles):
-    """Return name of the default profile, or raise `NoDefaultProfile`."""
+    """Return name of the default profile, or raise `NoDefaultProfile`.
+
+    :param profiles: The result of `ProfileConfig.open()`.
+    :return: The name of the default profile, or None if there is no
+        reasonable default.
+    """
     profiles_sample = list(islice(profiles, 2))
     if len(profiles_sample) == 1:
         # There's exactly one profile.  That makes a sensible default.
@@ -42,7 +53,12 @@ def name_default_profile(profiles):
 def select_profile(profiles, profile_name=None):
     """Return name for the applicable profile: the given name, or the default.
 
-    Returns None if no name was given and no default is available.
+    :param profiles: The result of `ProfileConfig.open()`.
+    :param profile_name: The profile requested by the user, if any.  This may
+        be `None`, in which case `select_profile` will look for a sensible
+        default to use.
+    :return: Name of the applicable profile, or `None` if no profile was
+        explicitly requested and no sensible default presents itself.
     """
     if profile_name is None:
         return name_default_profile(profiles)
