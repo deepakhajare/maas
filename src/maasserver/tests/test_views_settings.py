5885985c713232fd4cc587b9cb1f7457a328d869
Gavin Panella <gavin.panella@canonical.com>
2012-10-23 11:43:05 +0200

    Expose http_proxy config in the settings UI.

--- src/maasserver/tests/test_views_settings.py
+++ src/maasserver/tests/test_views_settings.py
@@ -82,6 +82,7 @@ class SettingsTest(AdminLoggedInTestCase):
         self.patch(settings, "DNS_CONNECT", False)
         new_name = factory.getRandomString()
         new_domain = factory.getRandomString()
+        new_proxy = "http://%s.example.com:1234/" % factory.getRandomString()
         response = self.client.post(
             reverse('settings'),
             get_prefixed_form_data(
@@ -89,12 +90,13 @@ class SettingsTest(AdminLoggedInTestCase):
                 data={
                     'maas_name': new_name,
                     'enlistment_domain': new_domain,
+                    'http_proxy': new_proxy,
                 }))
-
-        self.assertEqual(httplib.FOUND, response.status_code)
-        self.assertEqual(new_name, Config.objects.get_config('maas_name'))
-        self.assertEqual(
-            new_domain, Config.objects.get_config('enlistment_domain'))
+        get_config = Config.objects.get_config
+        self.assertEqual(httplib.FOUND, response.status_code, response.content)
+        self.assertEqual(new_name, get_config('maas_name'))
+        self.assertEqual(new_domain, get_config('enlistment_domain'))
+        self.assertEqual(new_proxy, get_config('http_proxy'))
 
     def test_settings_commissioning_POST(self):
         new_after_commissioning = factory.getRandomEnum(
