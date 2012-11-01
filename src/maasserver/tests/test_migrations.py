ee9c61df9751b8add9c042f9256ebc91a0568f56
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-11-01 12:06:58 +0100

    Test for duplicates in sequence numbering of South database migrations.

---
+++ src/maasserver/tests/test_migrations.py
@@ -0,0 +1,26 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Sanity checks for database migrations."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = []
+
+from maastesting.db_migrations import (
+    detect_sequence_clashes,
+    locate_migrations,
+    )
+from maastesting.testcase import TestCase
+
+
+class TestMigrations(TestCase):
+
+    def test_migrations_have_unique_numbers(self):
+        migrations_dir = locate_migrations(__file__)
+        self.assertEqual([], detect_sequence_clashes(migrations_dir))
