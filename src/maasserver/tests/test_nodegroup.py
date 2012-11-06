3d2bc6b7c38e882b230b8acf6ea48bccd538d977
Raphael Badin <raphael.badin@canonical.com>
2012-11-06 19:21:58 +0100

    Remove call to Mock().

--- src/maasserver/tests/test_nodegroup.py
+++ src/maasserver/tests/test_nodegroup.py
@@ -248,7 +248,7 @@ class TestNodeGroupManager(TestCase):
             (reload_object(nodegroup).status, changed_count))
 
     def test_import_pxe_files_accepted_clusters_calls_tasks(self):
-        recorder = self.patch(nodegroup_module, 'import_pxe_files', Mock())
+        recorder = self.patch(nodegroup_module, 'import_pxe_files')
         proxy = factory.make_name('proxy')
         Config.objects.set_config('http_proxy', proxy)
         accepted_nodegroups = [
