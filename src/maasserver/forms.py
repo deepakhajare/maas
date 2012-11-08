9c1ed61ac2bec98004b7aa77b8f35c000de79b9a
Raphael Badin <raphael.badin@canonical.com>
2012-11-08 09:33:59 +0100

    Update help text.

--- src/maasserver/forms.py
+++ src/maasserver/forms.py
@@ -158,7 +158,8 @@ class NodeForm(ModelForm):
             "host name: If the cluster controller for this node is managing "
             "DNS then the domain part in the host name (if any) is replaced "
             "by the domain defined on the cluster; if the cluster controller "
-            "does not manage DNS, then the host name is the FQDN."))
+            "does not manage DNS, then the host name as entered will be the "
+            "FQDN."))
 
     class Meta:
         model = Node
