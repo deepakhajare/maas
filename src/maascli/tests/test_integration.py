e9a327e03f96380e000cc6c98f17154d1237a895
Jeroen Vermeulen <jtv@canonical.com>
2012-11-19 17:12:25 +0100

    Get output from exception.

--- src/maascli/tests/test_integration.py
+++ src/maascli/tests/test_integration.py
@@ -44,13 +44,11 @@ class TestMAASCli(TestCase):
         self.output_file = self.make_file('output')
         try:
             self.run_command()
-        except CalledProcessError:
+        except CalledProcessError as e:
             pass
-        with open(self.output_file, 'rb') as captured_output:
-            output = captured_output.read()
         self.assertIn(
             "Run %s --help for usage details." % locate_maascli(),
-            output)
+            e.output)
 
     def test_help_option_succeeds(self):
         self.run_command('-h')
