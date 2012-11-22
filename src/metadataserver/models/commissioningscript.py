f5ec5828a187d751bf17a83c9262c2585e9503f5
Jeroen Vermeulen <jtv@canonical.com>
2012-11-22 12:53:01 +0100

    Drop custom create/read/update/delete code.

--- src/metadataserver/models/commissioningscript.py
+++ src/metadataserver/models/commissioningscript.py
@@ -18,50 +18,10 @@ __all__ = [
 
 from django.db.models import (
     CharField,
-    Manager,
     Model,
     )
 from metadataserver import DefaultMeta
 from metadataserver.fields import BinaryField
-
-
-class CommissioningScriptManager(Manager):
-    """Manager for `CommissioningScript`.
-
-    Don't import or instantiate this directly; access as
-    `CommissioningScript.objects`.
-    """
-
-    def store_script(self, name, content):
-        """Store a commissioning script.
-
-        :param name: Name for this commissioning script.  By convention, the
-            name should start with a two-digit number.  The prefix "00-maas-"
-            is reserved for MAAS-internal scripts.
-        :type name: :class:`unicode`
-        :param content: Binary script content.
-        :type content: :class:`metadataserver.fields.Bin`
-        :return: :class:`CommissioningScript` object.  If a script of the
-            given `name` already existed, it will be updated with the given
-            `content`.  Otherwise, it will be newly created.
-        """
-        script, created = self.get_or_create(
-            name=name, defaults={'content': content})
-        if not created:
-            script.content = content
-            script.save()
-        return script
-
-    def get_scripts(self):
-        """Return all :class:`CommissioningScript` objects, sorted by name.
-
-        The ordering is used to ensure a predictable execution order.
-        """
-        return self.order_by('name')
-
-    def drop_script(self, name):
-        """Delete the named :class:`CommissioningScript`, if it existed."""
-        self.filter(name=name).delete()
 
 
 class CommissioningScript(Model):
@@ -73,8 +33,6 @@ class CommissioningScript(Model):
 
     class Meta(DefaultMeta):
         """Needed for South to recognize this model."""
-
-    objects = CommissioningScriptManager()
 
     name = CharField(max_length=255, null=False, editable=False, unique=True)
     content = BinaryField(null=False)
--- src/metadataserver/tests/test_commissioningscript.py
+++ src/metadataserver/tests/test_commissioningscript.py
@@ -15,7 +15,6 @@ __all__ = []
 import codecs
 from random import randint
 
-from maasserver.testing import reload_object
 from maasserver.testing.factory import factory
 from maasserver.testing.testcase import TestCase
 from metadataserver.fields import Bin
@@ -32,95 +31,13 @@ def make_script_name(base_name=None, number=None):
         '%0.2d-%s' % (number, factory.make_name(base_name)))
 
 
-def make_script_content(recognizable_text='script'):
-    """Make up content for a commissioning script."""
-    text = "%s-%s" % (recognizable_text, factory.getRandomString())
-    return Bin(text.encode('ascii'))
-
-
-def list_names(scripts):
-    """List the respective names for `scripts`."""
-    return [script.name for script in scripts]
-
-
 class TestCommissioningScript(TestCase):
-
-    def test_store_script_creates_script(self):
-        name = make_script_name()
-        content = make_script_content()
-        CommissioningScript.objects.store_script(name, content)
-        stored_script = CommissioningScript.objects.get(name=name)
-        self.assertEqual(content, stored_script.content)
-
-    def test_store_script_overwrites_script(self):
-        name = make_script_name()
-        CommissioningScript.objects.create(
-            name=name, content=make_script_content('old'))
-        new_content = make_script_content('new')
-        CommissioningScript.objects.store_script(name, new_content)
-        stored_script = CommissioningScript.objects.filter(name=name)
-        self.assertEqual(1, len(stored_script))
-        self.assertEqual(new_content, stored_script[0].content)
-
-    def test_get_scripts_returns_nothing_if_no_scripts_defined(self):
-        self.assertItemsEqual([], CommissioningScript.objects.get_scripts())
-
-    def test_get_scripts_retrieves_all_scripts(self):
-        script = CommissioningScript.objects.store_script(
-            make_script_name(), make_script_content())
-        self.assertItemsEqual(
-            [script], CommissioningScript.objects.get_scripts())
-
-    def test_get_scripts_ignores_overwritten_scripts(self):
-        name = make_script_name()
-        CommissioningScript.objects.store_script(
-            name, make_script_content('old'))
-        new_content = make_script_content('new')
-        stored_script = CommissioningScript.objects.store_script(
-            name, new_content)
-        self.assertItemsEqual(
-            [stored_script],
-            CommissioningScript.objects.get_scripts())
-        self.assertEqual(new_content, reload_object(stored_script).content)
-
-    def test_get_scripts_orders_by_name(self):
-        names = [make_script_name(number=number) for number in [99, 1, 25, 8]]
-        for name in names:
-            CommissioningScript.objects.store_script(
-                name, make_script_content(name))
-        self.assertEqual(
-            sorted(names),
-            list_names(CommissioningScript.objects.get_scripts()))
-
-    def test_drop_script_removes_script(self):
-        name = make_script_name()
-        CommissioningScript.objects.store_script(name, make_script_content())
-        CommissioningScript.objects.drop_script(name)
-        self.assertItemsEqual([], CommissioningScript.objects.get_scripts())
-
-    def test_drop_script_leaves_other_scripts_alone(self):
-        doomed_script = make_script_name('doomed')
-        unaffected_script = make_script_name('unaffected')
-        CommissioningScript.objects.store_script(
-            doomed_script, make_script_content(doomed_script))
-        CommissioningScript.objects.store_script(
-            unaffected_script, make_script_content(unaffected_script))
-        CommissioningScript.objects.drop_script(doomed_script)
-        self.assertIn(
-            unaffected_script,
-            list_names(CommissioningScript.objects.get_scripts()))
-
-    def test_drop_script_tolerates_nonexistent_script(self):
-        CommissioningScript.objects.drop_script(
-            make_script_name('nonexistent'))
-        # The test is that we get here without errors.
-        pass
 
     def test_scripts_may_be_binary(self):
         name = make_script_name()
         # Some binary data that would break just about any kind of text
         # interpretation.
         binary = Bin(codecs.BOM64_LE + codecs.BOM64_BE + b'\x00\xff\x00')
-        CommissioningScript.objects.store_script(name, binary)
-        [stored_script] = CommissioningScript.objects.get_scripts()
+        CommissioningScript.objects.create(name=name, content=binary)
+        stored_script = CommissioningScript.objects.get(name=name)
         self.assertEqual(binary, stored_script.content)
