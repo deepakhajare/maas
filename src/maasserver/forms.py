d9613e9eaf46e8b2b4a1efa85cae6517b1b68283
Jeroen Vermeulen <jtv@canonical.com>
2012-12-20 14:55:41 +0700

    Implement checks.

--- src/maasserver/forms.py
+++ src/maasserver/forms.py
@@ -879,6 +879,10 @@ class CommissioningScriptForm(forms.Form):
     def clean_content(self):
         content = self.cleaned_data['content']
         name = content.name
+        if re.search('\s', name):
+            raise forms.ValidationError("Name contains whitespace.")
+        if re.search('["\']', name):
+            raise forms.ValidationError("Name contains quote or apostrophe.")
         if CommissioningScript.objects.filter(name=name).exists():
             raise forms.ValidationError(
                 "A script with that name already exists.")
